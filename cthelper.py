cf82f23bab0dd9bb81f3793a27a5b246f121bef8
Michael Fienen <mnfienen@usgs.gov>
2015-05-05 07:27:00 -0400

    Added to cthelper.py more helper functions to convert between single and double precision numpy array and pointer

--- cthelper.py
+++ cthelper.py
@@ -23,4 +23,22 @@ def c_double_p2float(cp, numels):
     numel --> is the number of elements of the array to return
     ''' 
     return ct.cast(cp, ct.POINTER(ct.c_double))[0:numels]
-    
+
+
+# convert a numpy single precision array to a ctypes single pointer
+def float32_to_c_float_p(carray):
+    '''
+    carray --> is a numpy single precision array
+    cp --> is a ctypes pointer to the single precision array
+    '''
+    cp = carray.ctypes.data_as(ct.POINTER(ct.c_float))
+    return cp
+
+# convert a numpy double precision array to a ctypes double pointer
+def double_to_c_float_p(carray):
+    '''
+    carray --> is a numpy double precision array
+    cp --> is a ctypes pointer to the single precision array
+    '''
+    cp = carray.ctypes.data_as(ct.POINTER(ct.c_double))
+    return cp
